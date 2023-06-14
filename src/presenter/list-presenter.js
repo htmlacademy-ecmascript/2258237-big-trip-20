f3e17066832fb84a8cfe05547e4c8fd5bfb70be4
Vladimir <troyanskii1998@yandex.ru>
2023-06-15 02:38:24 +0300

    добавляет сортировку точек маршрута

--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -1,16 +1,23 @@
 import { PointPresenter } from './point-presenter.js';
 import { ListView } from '../view/list.js';
+import { SortingView } from '../view/sort.js';
 import { NoPointsView } from '../view/no-points.js';
-import { render } from '../framework/render.js';
+import { render, RenderPosition } from '../framework/render.js';
 import { updateItem } from '../utils.js';
+
+import { SortType } from '../const.js';
+import { sortPointsByDate, sortPointsByTime, sortPointsByPrice } from '../utils/sort.js';
 
 
 export class ListPresenter {
   #listContainer = null;
   #pointsModel = null;
+  #sortComponent = null;
 
   #tripList = new ListView();
   #noPointsComponent = new NoPointsView();
+
+  #currentSortType = SortType.DAY;
 
   #listPoints = null;
   #offers = null;
@@ -28,6 +35,7 @@ export class ListPresenter {
     this.#offers = [...this.#pointsModel.offers];
     this.#destinations = [...this.#pointsModel.destinations];
 
+    this.#renderSort();
     this.#renderPointsList();
   }
 
@@ -49,6 +57,45 @@ export class ListPresenter {
   }
 
 
+  #sortPoints(sortType) {
+    switch (sortType) {
+      case SortType.DAY:
+        this.#listPoints.sort(sortPointsByDate);
+        break;
+      case SortType.EVENT:
+        break;
+      case SortType.TIME:
+        this.#listPoints.sort(sortPointsByTime);
+        break;
+      case SortType.PRICE:
+        this.#listPoints.sort(sortPointsByPrice);
+        break;
+      case SortType.OFFERS:
+        break;
+    }
+
+    this.#currentSortType = sortType;
+  }
+
+  #handleSortTypeChange = (sortType) => {
+    if (this.#currentSortType === sortType) {
+      return;
+    }
+
+    this.#sortPoints(sortType);
+    this.#clearPointList();
+    this.#renderPoints();
+  };
+
+  #renderSort() {
+    this.#sortComponent = new SortingView({
+      onSortTypeChange: this.#handleSortTypeChange,
+    });
+
+    render(this.#sortComponent, this.#listContainer, RenderPosition.BEFOREEND);
+  }
+
+
   #clearPointList() {
     this.#pointPresenters.forEach((presenter) => presenter.destroy());
     this.#pointPresenters.clear();
@@ -65,13 +112,18 @@ export class ListPresenter {
     this.#pointPresenters.get(updatedPoint.id).init(updatedPoint, this.#offers, this.#destinations);
   };
 
+  #renderPoints() {
+    for (let i = 0; i < this.#listPoints.length; i++) {
+      this.#renderPoint(this.#listPoints[i]);
+    }
+  }
 
   #renderPointsList() {
     render(this.#tripList, this.#listContainer);
 
-    for (let i = 0; i < this.#listPoints.length; i++) {
-      this.#renderPoint(this.#listPoints[i]);
-    }
+    this.#listPoints.sort(sortPointsByDate);
+
+    this.#renderPoints();
 
     if (this.#listPoints.length === 0) {
       this.#renderNoPoints();
---
+++ src/utils/sort.js
@@ -0,0 +1,17 @@
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+dayjs.extend(duration);
+
+function sortPointsByDate (a, b) {
+  return dayjs(a.dateFrom) > dayjs(b.dateFrom) ? 1 : -1;
+}
+
+function sortPointsByTime (a, b) {
+  return dayjs(a.dateFrom).diff(dayjs(a.dateTo)) > dayjs(b.dateFrom).diff(dayjs(b.dateTo)) ? 1 : -1;
+}
+
+function sortPointsByPrice (a, b) {
+  return a.basePrice > b.basePrice ? -1 : 1;
+}
+
+export {sortPointsByDate, sortPointsByTime, sortPointsByPrice};
